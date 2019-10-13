4c013210d375de9ff1493fc2092522f274acdf20
Vibrat <nguyenhoanglam229@gmail.com>
2019-10-13 19:03:12 +0700

    fixed error shortcode not contains GET variable

--- setup/source/wp-content/plugins/woo-form-checkout/front/product.shortcode.php
+++ setup/source/wp-content/plugins/woo-form-checkout/front/product.shortcode.php
@@ -8,15 +8,18 @@ function reg_product_checkout_by_form($atts= [], $content = null, $tag = '') {
 
   // override default attributes with user attributes
   $wporg_atts = shortcode_atts(
-    ['product_id' => $_GET['product_id'] ? $_GET['product_id'] : null ],
+    ['product_id' => isset($_GET['product_id']) ? $_GET['product_id'] : '' ],
     $atts,
     $tag);
 
   $product = wc_get_product($wporg_atts['product_id']);
+  
+  if ($product) {
+    ob_start();
+    return get_the_post_thumbnail($product->get_id(), 'img-product');
+  }
 
-  ob_start();
-
-  return get_the_post_thumbnail($product->get_id(), 'img-product');;
+  return false;
 }
 
 add_shortcode( 'checkout_product', 'reg_product_checkout_by_form' );
